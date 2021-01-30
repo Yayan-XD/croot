e9903b07f98dabd611f644267e06c0c7fbbe086a
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-01-30 10:09:57 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+pip install requests
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip install mechanize
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip install requests bs4
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pkg install figlet
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip2 install requests
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip2 install tqdm
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pip2 install mechanize
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+pkg install ruby -y
+clear
+echo
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+echo
+gem install lolcat
+clear
+echo "   ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗  ╦ ╦╔═╗╦╔╦╗" | lolcat
+echo "   ╠═╝║  ║╣ ╠═╣╚═╗║╣   ║║║╠═╣║ ║ " | lolcat
+echo "   ╩  ╩═╝╚═╝╩ ╩╚═╝╚═╝  ╚╩╝╩ ╩╩ ╩ " | lolcat
+clear
+python2 lib/ngentod.py
